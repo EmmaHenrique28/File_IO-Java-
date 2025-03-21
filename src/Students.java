42c398015c8d3490bbee1d7c23707267c73a7125
EmmaHenrique <Bolandjowe@gmail.com>
2025-03-21 15:13:34 +0200

    Class constructor to accept the full filepath

---
+++ src/Students.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Students{
+    
+    File student_file;
+
+    //Constructor
+    public Students(String filepath){
+
+        student_file = new File(filepath);
+    }
+
+    
+}
